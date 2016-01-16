1c18f1099146a8536f25d05a5e1e8aca20535db4
lemitatus <janreich2@gmail.com>
2016-01-16 14:38:20 +0100

    Changelog: Main & startposition
    - mainfunktion überarbeitet und funktionen angepasst
    - startpostion initialisiert
    - hauptmenu überarbeitet

--- Main.c
+++ Main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
+
 #define feld 10
 #define frei 254
 #define p1 184
@@ -15,35 +17,38 @@ void hauptmenu ()
     switch (x)
     {
         case 1:
+            ausgabe_konsole();
             spielschleife();
             break;
-        case 2:
+   /*     case 2:
             design();
-            break;
+            break;  */
         case 3:
             return;
-            
+
         default:
             break;
     }
-    
+
 }
-
-
 void spielschleife()
 {
+    int end=1, player=1;
+
+    printf("\nEingabe: ");
+
     while (end!=0)
     {
         switch (player)
         {
             case 1:
-                spielzug();
-                player=2
+                spielzug(player);
+                player=2;
                 break;
             case 2:
-                spielzug();
-                player=1
-                
+                spielzug(player);
+                player=1;
+
             default:
                 break;
         }
@@ -53,8 +58,6 @@ void spielschleife()
         }
     }
 }
-
-
 
 int check_win()
 {
@@ -127,8 +130,25 @@ for (y=0; y<=8; y++){                   /* Ausgabe des Arrays mit ASCII-Variable
 }
 
 }/*void ausgabe_konsole*/
+void startposition () {
+    int x, i;
+    i=1;
+    for (x=0; x<4; x++){           /* Player 1 Steine setzen */
+        field[i+1][1]=p1;
+        field[i][2]=p1;
+        field[i+1][3]=p1;
+        i=i+2;
+    }
+    i=1;
+    for (x=0; x<4; x++){            /* Player 2 Steine setzen */
+        field[i][6]=p2;
+        field[i+1][7]=p2;
+        field[i][8]=p2;
+        i=i+2;
+    }
 
 
+}
 void spielzug(int player) {
 
   int start_1, ziel_1;
@@ -138,111 +158,121 @@ void spielzug(int player) {
     printf("Spielstein waehlen\n");
     int go=1;
 
-    while (go!=0) { //Spielstein?
-      scanf("%c %d",&start_a ,&start_1);//Buchstabe Zahl
-      start_a_i=start_a-96; //Buchstabe in Koordinatenzahl umgerechnet
+    while (go!=0) { /* Spielstein? */
+      scanf("%c",&start_1);/* Buchstabe Zahl */
+      scanf("%d",&start_a);/* Buchstabe Zahl */
+      start_a_i=start_a-96; /* Buchstabe in Koordinatenzahl umgerechnet */
 
       if (player == 1 && field [start_a_i][start_1]==p1) {
-        go=1;
+        go=0;
       }
       else if (player == 2 && field [start_a_i][start_1]==p2) {
-        go=1;
+        go=0;
       }
       else
       printf("Ausgewaehltes Feld enthaelt nicht ihren Spielstein, bitte neues Feld aussuchen\n");
-      }//while go
+      }/* while go */
 
     printf("Zielsposition waehlen\n");
     go=1;
 
     while (go!=0) { /*so lange Zielfeld waehlen, bis Zug moeglich ist*/
-    scanf("%c %d",&ziel_a ,&ziel_1);
-    ziel_a_i=ziel_a-96; //Buchstabe in Koordinatenzahl umgerechnet
-    int check=(ziel_a_i+ziel_1) % 2;//Feldwerte addiert muss ungerade sein
+    scanf("%c",&start_1);/* Buchstabe Zahl */
+    scanf("%d",&start_a);/* Buchstabe Zahl */
+    ziel_a_i=ziel_a-96; /* Buchstabe in Koordinatenzahl umgerechnet */
+    int check=(ziel_a_i+ziel_1) % 2;/* Feldwerte addiert muss ungerade sein */
 
-    if (check!=0 || ziel_a_i<8 || ziel_1<8){//außerhalb oder wißes Feld
+    if (check!=0 || ziel_a_i<8 || ziel_1<8){/*außerhalb oder weißes Feld*/
       printf("Feld ausserhalb des bespielbaren Feldes, neuer Versuch!\n");
     }
 
-    if (player==1 && field [ziel_a_i][ziel_1]==frei){ //gegenchecken richtiges leeres feld?...einfacher, möglicher Zug
+    if (player==1 && field [ziel_a_i][ziel_1]==frei){ /* gegenchecken richtiges leeres feld?...einfacher, möglicher Zug */
       if (ziel_1==start_1+1 && ziel_a_i==start_a_i+1){
         field [ziel_a_i][ziel_1]=p1;
         field [start_a_i][start_1]=frei;
-        go=1;
+        ausgabe_konsole();
+        go=0;
       }
       else if (ziel_1==start_1-1 && ziel_a_i==start_a_i+1){
         field [ziel_a_i][ziel_1]=p1;
         field [start_a_i][start_1]=frei;
-        go=1;
+        ausgabe_konsole();
+        go=0;
       }
-    }//if player1
+    }/* if player1 */
 
-    if (player==2 && field [ziel_a_i][ziel_1]==frei){ //gegenchecken richtiges leeres feld?...einfacher, möglicher Zug
+    if (player==2 && field [ziel_a_i][ziel_1]==frei){ /* gegenchecken richtiges leeres feld?...einfacher, möglicher Zug */
       if (ziel_1==start_1+1 && ziel_a_i==start_a_i-1){
         field [ziel_a_i][ziel_1]=p2;
         field [start_a_i][start_1]=frei;
-        go=1;
+        ausgabe_konsole();
+        go=0;
       }
       else if (ziel_1==start_1-1 && ziel_a_i==start_a_i-1){
         field [ziel_a_i][ziel_1]=p2;
         field [start_a_i][start_1]=frei;
-        go=1;
+        ausgabe_konsole();
+        go=0;
       }
-    }//if player2
+    }/* if player2 */
 
-    if (player==1 && field [ziel_a_i][ziel_1]==p2){ //Treffer
-      if (ziel_1==start_1+1 && ziel_a_i==start_a_i+1 && field [ziel_a_i+1][ziel_1+1]==frei){//rechts unten
+    if (player==1 && field [ziel_a_i][ziel_1]==p2){ /* Treffer */
+      if (ziel_1==start_1+1 && ziel_a_i==start_a_i+1 && field [ziel_a_i+1][ziel_1+1]==frei){/* rechts unten */
         field [ziel_a_i][ziel_1]=frei;
         field [start_a_i][start_1]=frei;
         field [ziel_a_i+1][ziel_1+1]=p1;
+        ausgabe_konsole();
         printf("Weiterer Zug? Ja=1 Nein=0\n");
         int a;
         scanf("%d",&a);
         if (a==1)
           spielzug(player);
         else
-        go=1;
+        go=0;
       }
-      else if (ziel_1==start_1-1 && ziel_a_i==start_a_i+1 && field [ziel_a_i+1][ziel_1-1]==frei){//links unten
+      else if (ziel_1==start_1-1 && ziel_a_i==start_a_i+1 && field [ziel_a_i+1][ziel_1-1]==frei){ /* links unten */
         field [ziel_a_i][ziel_1]=frei;
         field [start_a_i][start_1]=frei;
         field [ziel_a_i+1][ziel_1-1]=p1;
+        ausgabe_konsole();
         printf("Weiterer Zug? Ja=1 Nein=0\n");
         int a;
         scanf("%d",&a);
         if (a==1)
           spielzug(player);
         else
-        go=1;
+        go=0;
       }
-    }//if player1
+    }/* if player1 */
 
     if (player==2 && field [ziel_a_i][ziel_1]==p1){ /*Treffer*/
       if (ziel_1==start_1+1 && ziel_a_i==start_a_i-1 && field [ziel_a_i-1][ziel_1+1]==frei){ /*rechts oben */
         field [ziel_a_i][ziel_1]=frei;
         field [start_a_i][start_1]=frei;
         field [ziel_a_i-1][ziel_1+1]=p2;
+        ausgabe_konsole();
         printf("Weiterer Zug? Ja=1 Nein=0\n");
         int a;
         scanf("%d",&a);
         if (a==1)
           spielzug(player);
         else
-        go=1;
+        go=0;
       }
       else if (ziel_1==start_1-1 && ziel_a_i==start_a_i-1 && field [ziel_a_i-1][ziel_1-1]==frei){ /*links oben */
         field [ziel_a_i][ziel_1]=frei;
         field [start_a_i][start_1]=frei;
         field [ziel_a_i-1][ziel_1-1]=p2;
+        ausgabe_konsole();
         printf("Weiterer Zug? Ja=1 Nein=0\n");
         int a;
         scanf("%d",&a);
         if (a==1)
           spielzug(player);
         else
-        go=1;
+        go=0;
       }
-    }//if player2
+    }/*if player2*/
   }/*while go Ziel*/
 }/*void spielzug*/
 
@@ -250,25 +280,10 @@ void spielzug(int player) {
 int main () {
 int player;
 int end;
-hauptmenu();
-feldaufbau();
 
-ausgabe_konsole();
-ausgabe_bitmap();
-
-spielschleife();
-eingabe_konsole();
-spielzug();
-check_win();
-    
 spielfeld();
-ausgabe_konsole();
-while (end!=0){
-  player=1;
-  spielzug(player);
-  player=2;
-  spielzug(player);
-}
+startposition();
+hauptmenu();
 
 return 0;
 }
